ca46e1c4333e7f7e2bdc7bfec9cad1873dfa988b
javenkn <javenkn7@yahoo.com>
2019-05-12 20:58:34 +0900

    adds semantic ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import 'semantic-ui-css/semantic.min.css';
 
 import Routes from './routes';
 
